10be4d4bb17d0da279e2d3f6c43a161e8a99162b
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2023-12-04 01:51:50 +0530

    new

---
+++ 8086/uppertolower.asm
@@ -0,0 +1,40 @@
+
+DATA SEGMENT
+    STRING1 DB 0AH,0DH,"ENTER STRING $"
+    STRING2 DB 6,0,6 DUP(?)
+DATA ENDS
+CODE SEGMENT
+START:
+    MOV AX,DATA
+    MOV DS,AX
+    LEA DX,STRING1
+    MOV AH,09H
+    INT 21H
+    LEA DX,STRING2
+    MOV AH,0AH
+    INT 21H
+    LEA SI,STRING2
+    MOV CX,05H
+LOOP:
+    MOV AX,[SI]
+    SUB AX, 'A'
+    ADD AX,'a'
+    MOV [SI],AX
+    CMP CX,00H
+    INC SI
+    DEC CX
+    JNZ LOOP
+    JMP EXIT
+EXIT:  
+    LEA DX,STRING2
+    MOV AH,09H
+    INT 21H
+    MOV AH,4CH
+    INT 21H       
+    
+END START    
+CODE ENDS  
+
+
+
+
